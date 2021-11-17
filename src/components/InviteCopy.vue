5cbbb6355470984c9e0cdf6fdfbd0c9bdfd6df92
Timothy Tjong-Ayong <timothy.tjong-ayong@student.hs-rm.de>
2021-11-17 12:15:50 +0100

    Komponente zum Kopieren eines Links hinzufegügt

---
+++ src/components/InviteCopy.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-cloak>
+        <h4 class="center">Lade deine Freunde ein!</h4>
+        <div class=container>
+            <div class="row">
+            <div class="form-control">
+                <span class="title">{{linkCopy}}</span>
+                <button class="btn btn-info text-white" v-on:click="copyInviteLink">Link kopieren</button>
+                <input type="hidden" id="link-copy" :value="linkCopy">
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+    data: () => {
+        return {
+        //TODO Link für Lobby
+        linkCopy: "https://dummy-link.copy"
+        }
+    },
+
+    methods: {
+        //Kopiert den Text aus linkCopy 
+        copyInviteLink(){
+            try {
+                navigator.clipboard.writeText(this.linkCopy)
+            } catch (err) {
+                console.error("Link konnte nicht kopiert werden")
+            }
+        }
+    }
+});
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -4,14 +4,19 @@
       <button class="btn btn-primary" @click="$router.push('/')">
         Home
       </button>
+      <InviteCopy/>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue';
+import InviteCopy from '@/components/InviteCopy.vue'
+
 export default defineComponent({
   name: 'Create',
+  components: { InviteCopy },
 });
+
 </script>
 
 <style scoped>
