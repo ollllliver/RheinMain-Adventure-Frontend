f4d1d2e8faa36ea1badb5f87aed649793aea7ab3
Oliver Renth <oliver.renth@gmail.com>
2021-11-18 05:33:17 +0100

    STOMP Message Broker lauffähig bekommen für die Kommunikation mit dem Backend 🍻

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
       <h1> Create Lobby Page </h1>
+      <button class="btn btn-primary" v-on:click="create">
+        Create Lobby
+      </button>
+      <br>
+      <br>
       <button class="btn btn-primary" @click="$router.push('/')">
         Home
       </button>
@@ -8,10 +13,52 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue';
-export default defineComponent({
-  name: 'Create',
-});
+  import { defineComponent } from 'vue';
+  import { Client, Message } from '@stomp/stompjs';
+  export default defineComponent({
+    name: 'Create',
+    setup() {
+      // Absolute WS-URL zusammensetzen, Host/Port wie Frontend-Anw.
+      const wsurl = `ws://localhost:8080/messagebroker`;
+      const stompclient = new Client({ brokerURL: wsurl })
+      stompclient.onWebSocketError = (event) => { /* WS-Fehler */ }
+      stompclient.onStompError = (frame) => { /* STOMP-Fehler */ }
+      stompclient.onConnect = (frame) => {
+        // Callback: erfolgreicher Verbindugsaufbau zu Broker
+        console.log("Erfolgreich verbunden: " + frame);
+        stompclient.subscribe("/topic/lobby/create", (message) => {
+          // Callback: Nachricht auf DEST empfangen
+          // empfangene Nutzdaten in message.body abrufbar,
+          // ggf. mit JSON.parse(message.body) zu JS konvertieren
+          console.log("message from broker:", message.body);
+
+        });
+      };
+      stompclient.onDisconnect = () => { /* Verbindung abgebaut*/ }
+
+      // Verbindung zum Broker aufbauen
+      stompclient.activate();
+
+      function create() {
+        const DEST = "/topic/lobby/create";
+        console.log("create")
+        // Nachrichtenversand vom Client zum Server
+        try {
+          stompclient.publish({ destination: "/topic/lobby/create", headers: {},
+          body: "Hier vielleicht der Username fuer das hashen der LobbyID"
+          // ... oder body: "irgendein String"
+        });
+        } catch (fehler) {
+          // Problem beim Senden
+        }
+      }
+
+      return {
+        create
+      }
+
+    }
+  });
 </script>
 
 <style scoped>
