8a31d2d013732ede839fc059affecfccfa27c3d6
jgrae001 <julian.graeber@student.hs-rm.de>
2021-11-19 11:46:34 +0100

    #20 login/register request von local auf backend

--- src/requests.ts
+++ src/requests.ts
@@ -7,19 +7,59 @@ export async function getUser() {
   return user
 }
 
+// POST /login -> User wird überprüft in DB und eingeloggt falls erfolgreich 
 export async function login(username: string, password: string) {
-  const request = await fetch('/users.json')
+  const user = {
+    username: username,
+    password: password
+  }
+  await fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true'
+    },
+     body: JSON.stringify(user)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+  return user
+  /*const request = await fetch('/users.json')
   const usersJson: UserList = await request.json()
-  return usersJson.find(s => s.password === password && s.username === username)
+  return usersJson.find(s => s.password === password && s.username === username)*/
 }
 
+// POST /register -> User wird in der Datenbank angelegt
 export async function signup(username:string, password: string) {
-  const request = await fetch('/users.json')
-  const userJson: UserList = await request.json()
-  if (userJson.find(s => s.username === username)) {
-    return null
+  const user = {
+    username: username,
+    password: password,
+    online: false
   }
+  await fetch('http://localhost:8080/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true'
+    },
+    body: JSON.stringify(user)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
   
-  userJson.push({username, password})
-  return userJson
+  return user
 }
+ 
