f171fbb3fd9d6636300921ae2e6acdc4b1e94de1
Friedrich Roskosch <Friedrich.Roskosch@student.hs-rm.de>
2021-12-15 13:12:41 +0100

    Vue-Router vereinheitlicht

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,44 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Home from '../views/Home.vue'
 import LobbyView from '@/views/LobbyView.vue'
+import Register from "@/views/Register.vue";
+import LobbyuebersichtView from "@/views/LobbyuebersichtView.vue";
+import Editor from "@/views/Editor.vue";
 
 // Routen der Anwendung
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: () => import('../views/Register.vue')
-  },
-  {
-    path: '/uebersicht',
-    name: 'Lobbyuebersicht',
-    component: () => import('../views/LobbyuebersichtView.vue')
-  },
-  {
-    path: '/lobby/:lobby_id',
-    name: 'LobbyView',
-    component: LobbyView,
-    props:true
-  },
-  {
-    path: '/editor',
-    name: 'Editor',
-    component: () => import('../views/Editor.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/signup',
+        name: 'SignUp',
+        component: Register
+    },
+    {
+        path: '/uebersicht',
+        name: 'Lobbyuebersicht',
+        component: LobbyuebersichtView
+    },
+    {
+        path: '/lobby/:lobby_id',
+        name: 'LobbyView',
+        component: LobbyView,
+        props: true
+    },
+    {
+        path: '/editor',
+        name: 'Editor',
+        component: Editor
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
