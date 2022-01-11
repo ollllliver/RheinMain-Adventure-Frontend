575f2d7ac890544347a7e288acc531276c1db32f
Pakisa Naurozi <pakisa.naurozi@student.hs-rm.de>
2022-01-11 19:00:41 +0100

    Editorübersicht fertig

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/main.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 
 createApp(App).use(router).mount('#app')
