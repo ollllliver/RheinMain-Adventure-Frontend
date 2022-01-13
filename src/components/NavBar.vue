7aa59bfcb21919cb037e2aa58781d4ab87bf9a48
Oliver Renth <oliver.renth@gmail.com>
2022-01-13 18:38:56 +0100

    init Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-light p-3 border-bottom border-success" style="background-color: #E0F6DD;">
+    <a class="navbar-brand" href="#">
+        <img src="favicon.ico" width="30" height="30" class="d-inline-block align-top" alt="">
+        RheinMain-Adventure
+    </a>
+    <div>
+        Aktuelle seite
+    </div>
+    <button class="btn btn-success my-2 my-sm-0" type="submit">Logout</button>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NavBar",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style>
+</style>
---
+++ src/stores/navbarStore.ts
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+export const navbarbar = reactive({
+    title: "",
+    buttonText: "",
+    button: false,
+})
