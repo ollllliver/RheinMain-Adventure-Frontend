32c5d2bebd383964d49d7224392410489872f7c5
Pakisa Naurozi <pakisa.naurozi@student.hs-rm.de>
2022-01-21 16:27:04 +0100

    Landingpage mit neuem bild

--- src/views/Landingpage.vue
+++ src/views/Landingpage.vue
@@ -1,93 +1,45 @@
 <!-- View für die Landingpage -->
 
 <template>
-
 <div id="box" class="container my-5">
   <div class="row">
-    <div class="col-md-7"> 
-      <img src="../assets/img/logos/rma_logo.png" class="float-left" width="600" height="265">
+    <div id="mitte" class="col-md-7"> 
+      <h1>Willkommen bei<br /><span>Rhein-Main Adventure!</span></h1>
+        <p align="left" class="text">Du liebst Labyrinthe bauen und knackst sie am liebsten mit anderen? 
+        Dann bist du hier genau richtig!</p>
+          <button type="button" class=" btn-lg btn-block rounded-0" id="button" @click="$router.push('/')">Jetzt Anmelden</button>
     </div>
-    <div id="text" class="col-md-5">
-      <p class="mt-4">
-        Willkommen bei Rhein-Main-Adventure! 
-        Du möchtest spannende Labyrinthe lösen? Und dabei viele Gegenstände sammeln?
-        Dann bist du hier genau richtig!
-        Melde dich jetzt bei uns an.
-      </p>
-      <button type="button" class="btn btn-light" @click="$router.push('/')">ANMELDEN</button>
-    </div> 
-
-    <div id="blackRow">&nbsp;</div>
+    <div class="col-md-5">
+      <img src="../assets/img/logos/RMA_heart.png" class="float-left" width="400" height="400">
+     </div> 
   </div>
-
-  
-</div>
-
-<div id="kasten" class="container my-5">
-  <div class="row" >
-  <div class="col" >
-    <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <img class="card-img-top" src="../assets/img/logos/rma_icon.png" alt="Card image cap">
-        <h5 class="card-title">So funktioniert Rhein-Main Adventure</h5>
-        <p class="card-text">Drücke auf Anleitung um genaueres über unser Spiel zu erfahren.</p>
-        <button type="button" class="btn btn-secondary" @click="$router.push('/instructions')" >Anleitung</button>
-      </div>
-    </div>
-  </div>
-  <div class="col ">
-  </div>
-  <div class="col">
-    <div class="card" style="width: 18rem; ">
-      <div class="card-body">
-        <img class="card-img-top" src="../assets/img/logos/rma_icon.png" alt="Card image cap">
-        <h5 class="card-title">Starte jetzt dein persönliches Level!</h5>
-        <p class="card-text">Melde dich ein um direkt lozulegen. Viel Erfolg!</p>
-        <button type="button" class="btn btn-secondary" @click="$router.push('/')">Einloggen</button>
-      </div>
-    </div>
-  </div>
-</div>
 </div>
 
 </template>
-
 <script>
 export default {
-  
 }
 </script>
 
 
 <style scoped>
-#box{
-  background-color: #6d9466 ;
+#button{
+  background-color: #6d9466;
+  color: black;
+  text-decoration: none;
+  padding: 10px 60px;
+  font-size: 30px;
+  border: 2px solid black;
+  cursor: pointer;
+  margin: 30px;
 }
-#text{
-  color:white;
-  text-align: center;
-  
-}
-#blackRow {
-margin: 0;
-padding: 0;
-height: 20px;
-background-color: black;
-}
-
-
-#card-body{
-  
-  text-align: center;
-  
+#mitte{
+  color:black;
+  text-align: center; 
 }
 
-#kasten{
-
-
-display: flex;
-justify-content: center;
-
-}
-
+p.text {
+  margin: 35px;
+  font-size: 18px;
+  }
 </style> 
